0d94ac269d70b6d930c640a945c68b91c6f76a6e
ujeneza vanessa <vankessa@gmail.com>
2018-12-13 14:11:25 +0200

    added an array to store my inputs

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ var triangle =['Equilateral','Isosceles','Scalene'];
 var firstSide=prompt('Enter first side number ');
 var secondSide=prompt('Enter second side number');
 var thirdSide=prompt('enter third side number');
+var store=['firstSide','secondSide','thirdSide'];
 function side(firstSide,secondSide,thirdSide){
 if(firstSide===secondSide===thirdSide){
     console.log(triangle[0]); 
@@ -15,7 +16,7 @@ else if( ((firstSide!=secondSide)&&(firstSide!=thirdSide)&&(secondSide!=thirdSid
 else{
 
 }
-return triangle[0];
+
 
 
 }
