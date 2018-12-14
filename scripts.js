bd906dcdba959836fff79dd4076ec97b6e6c15ef
ujeneza vanessa <vankessa@gmail.com>
2018-12-14 11:57:58 +0200

    declared variables

--- scripts.js
+++ scripts.js
@@ -9,6 +9,13 @@ var Sum1 = function(value1, value2) {
     var Sum3 = function(value1, value3) {
     return value1 + value3;
     };
+    function check(){
+        var triangle=['equilaterial triangle','Isoscele triangle','Scalene triangle']
+     var value1=document.getElementById('one').value;
+     var value2=document.getElementById('two').value;
+     var value3=document.getElementById('three').value;
+     var text;
+    }
     
 
 
