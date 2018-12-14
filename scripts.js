6355a97e8d73f13c98c603444ee10baa8750965c
ujeneza vanessa <vankessa@gmail.com>
2018-12-14 14:21:32 +0200

    added background color

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-var Sum1 = function(value1, value2) {
+ var Sum1 = function(value1, value2) {
     return value1 + value2;
     };
     
@@ -15,7 +15,7 @@ var Sum1 = function(value1, value2) {
      var value2=document.getElementById('two').value;
      var value3=document.getElementById('three').value;
      var text;
-    if(Sum1(value1,value2) <= value3 || Sum2(value2,value3) <= value1 || Sum3(value1,value3) <= value2){
+     if(Sum1(value1,value2) <= value3 || Sum2(value2,value3) <= value1 || Sum3(value1,value3) <= value2){
         text="it is not a triangle"
     }
     else if(value1 === value2 && value2 === value3){
@@ -26,15 +26,15 @@ var Sum1 = function(value1, value2) {
      else if(value1===value2 || value1===value3 || value2===value3) {
         text=triangle[1];
     }
-    //Isoscele Triangle
-    else if(value1!==value2 && value1!==value3 &&value2!==value3){
+     //Isoscele Triangle
+     else if(value1!==value2 && value1!==value3 &&value2!==value3){
         text=triangle[2];
     }
     //Scalene Triangle
     else{
         text = "it is not a triangle.";
-    }
-    document.getElementById('demo').innerHTML = text;
+     }
+     document.getElementById('demo').innerHTML = text;
 }
     
 
