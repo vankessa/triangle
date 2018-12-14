fa9093d2a4f005fece873e16e78e454227da5be5
ujeneza vanessa <vankessa@gmail.com>
2018-12-14 11:56:19 +0200

    added functions to perform summation

--- scripts.js
+++ scripts.js
@@ -1,59 +1,15 @@
- function myFunction(){
-     var firstString1=document.getElementById("my number1").value;
-    alert(firstString1);
- 
-     var firstString2=document.getElementById("my number2").value;
-     alert(firstString2);
-    var firstString3=document.getElementById("my number3").value;
-    alert(firstString3);
-
+var Sum1 = function(value1, value2) {
+    return value1 + value2;
+    };
     
-var triangle =['Equilateral','Isosceles','Scalene'];
-if(firstString1+firstString2>firstString3){
-    Text="not a triangle";
-    // if(toGetEquilateral((firstString1===firstString2)&&(firstString1===firstString3)&&(firstString2===firstString3))){
-    //     alert(triangle[0]);
-    // }
-    // else {
-    //     alert("not a triangle");
-    // }
-
-}
-document.getElementById('demo').innerHTML = text;
- }
-
-
+    var Sum2 = function(value2, value3) {
+    return value2 + value3;
+    };
     
-
-
-
-// alert(side());
-         
-//  else if((firstSide===secondSide) && (firstSide!=thirdSide) && (secondSide!=thirdSide)){
-
-//         function side(firstSide,secondSide,thirdSide){
-//             return triangle[1];
-//         }
-//         alert(side());
-//     }
-//     else if((firstSide!=secondSide)&&(firstSide!=thirdSide)&&(secondSide!=thirdSide)&&((firstSide+secondSide)>thirdSide)){
-//         function side(firstSide,secondSide,thirdSide){
-//             return triangle[2];
-//         }
-//         alert(side());
-//     }
-//     else if((firstSide+secondSide)=thirdSide||(firstSide+secondSide)<thirdSide){
-        
-//         console.log("It is not a triangle");
-//     }
-//     else{
-//         console.log(" it is any other shape")
-//     }
-   
-//     return triangle[0];
+    var Sum3 = function(value1, value3) {
+    return value1 + value3;
+    };
     
-    
-
 
 
 
