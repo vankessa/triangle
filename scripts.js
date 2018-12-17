8808d6a5c2780c7c9411a02034b86c6be6d2879c
ujeneza vanessa <vankessa@gmail.com>
2018-12-17 14:26:43 +0200

    added content int read me file

--- scripts.js
+++ scripts.js
@@ -1,5 +1,5 @@
- var Sum1 = function(value1, value2) {
-    return value1 + value2;
+  var Sum1 = function(value1, value2) {
+   return value1 + value2;
     };
     
     var Sum2 = function(value2, value3) {
@@ -8,32 +8,41 @@
     
     var Sum3 = function(value1, value3) {
     return value1 + value3;
-    };
+   };
     function check(){
         var triangle=['equilaterial triangle','Isoscele triangle','Scalene triangle']
      var value1=document.getElementById('one').value;
      var value2=document.getElementById('two').value;
      var value3=document.getElementById('three').value;
      var text;
-     if(Sum1(value1,value2) <= value3 || Sum2(value2,value3) <= value1 || Sum3(value1,value3) <= value2){
-        text="it is not a triangle"
-    }
-    else if(value1 === value2 && value2 === value3){
+     if(Sum1(value1,value2) > value3 || Sum2(value2,value3) > value1 || Sum3(value1,value3) > value2){
+        // text="this values can't form a triangle"
+     if(value1 === value2 && value2 === value3){
         text = triangle[0];
     }
-     //Equivalent Triangle
+     //Equilateral Triangle
     
      else if(value1===value2 || value1===value3 || value2===value3) {
         text=triangle[1];
     }
      //Isoscele Triangle
-     else if(value1!==value2 && value1!==value3 &&value2!==value3){
+     else {
+     
+
+//  if ((value1!==value2 )&& (value1!==value3) &&(value2!==value3 )){
         text=triangle[2];
     }
+}
+
     //Scalene Triangle
+
     else{
         text = "it is not a triangle.";
      }
+
+    
+    
+    
      document.getElementById('demo').innerHTML = text;
 }
     
