fc08fb446c356c874a970b36ffe5d997fbdc1272
ujeneza vanessa <vankessa@gmail.com>
2018-12-17 15:29:33 +0200

    added BDD into read me file

--- scripts.js
+++ scripts.js
@@ -1,4 +1,4 @@
-  var Sum1 = function(value1, value2) {
+   var Sum1 = function(value1, value2) {
    return value1 + value2;
     };
     
@@ -7,18 +7,19 @@
     };
     
     var Sum3 = function(value1, value3) {
-    return value1 + value3;
-   };
+     return value1 + value3;
+  };
     function check(){
         var triangle=['equilaterial triangle','Isoscele triangle','Scalene triangle']
      var value1=document.getElementById('one').value;
      var value2=document.getElementById('two').value;
      var value3=document.getElementById('three').value;
+     
      var text;
      if(Sum1(value1,value2) > value3 || Sum2(value2,value3) > value1 || Sum3(value1,value3) > value2){
         // text="this values can't form a triangle"
      if(value1 === value2 && value2 === value3){
-        text = triangle[0];
+         text = triangle[0];
     }
      //Equilateral Triangle
     
@@ -26,25 +27,27 @@
         text=triangle[1];
     }
      //Isoscele Triangle
-     else {
+     
      
 
-//  if ((value1!==value2 )&& (value1!==value3) &&(value2!==value3 )){
+  else if ((value1!==value2 )&& (value1!==value3) &&(value2!==value3 )){
         text=triangle[2];
     }
-}
+    else {
+        text=" not a triangle";
+    }
+    
+     }
 
     //Scalene Triangle
 
-    else{
+    else {
         text = "it is not a triangle.";
      }
-
     
-    
-    
-     document.getElementById('demo').innerHTML = text;
+     document.getElementById('demo').innerHTML=text;
 }
+    
     
 
 
